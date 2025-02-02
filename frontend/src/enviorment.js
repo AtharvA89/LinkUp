2d53cb3dbac15c9648dbb202bd6edbbbd0510543
AtharvA89 <atharvkuratkar07@gmail.com>
2025-02-02 22:49:21 +0530

    V-new server added

--- frontend/src/enviorment.js
+++ frontend/src/enviorment.js
@@ -1,6 +1,6 @@
 let IS_PROD=true;//make it false for development
 const server= IS_PROD ?
-  "https://linkupbackend-0d8d.onrender.com" :
+  "https://linkupbackend-oj5y.onrender.com" :
   "http://localhost:8000"
 
   export default server;
